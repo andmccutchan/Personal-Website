d2eed373d98287667b17f50faea2c6f1179c816f
andmccutchan <andmccutchan2004@gmail.com>
2025-02-25 13:03:33 -0500

    Initial Commit

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjectCard = () => {
+  return (
+    <div className="">
+      <img src="" alt="" />
+      <p></p>
+      <button className="btn"></button>
+      <button className="btn"></button>
+    </div>
+  );
+};
+
+export default ProjectCard;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectCard from "../Components/ProjectCard";
+
+function Home() {
+  return (
+    <div>
+      <ProjectCard />
+    </div>
+  );
+}
+
+export default Home;
