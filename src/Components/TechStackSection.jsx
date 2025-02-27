36fad4485c16de9a8b858ad9fd483f5b8d3e9388
andmccutchan <andmccutchan2004@gmail.com>
2025-02-26 21:44:37 -0500

    Texk stack section complete for the most part

--- src/Components/TechStackSection.jsx
+++ src/Components/TechStackSection.jsx
@@ -5,63 +5,63 @@ const TechStackSection = () => {
   return (
     <div className="w-full secondary-font text-5xl text-stone-50">
       <h2>Tech Stack</h2>
-      <Marquee>
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-          height="60"
-          width="60"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
-          height="60"
-          width="60"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-          width="60"
-          height="60"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
-          width="60"
-          height="60"
-        />
-
-        <img
-          className="bg-stone-50"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
-          width="60"
-          height="60"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
-          width="60"
-          height="60"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
-          width="60"
-          height="60"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-          width="60"
-          height="60"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
-          width="60"
-          height="60"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
-          width="60"
-          height="60"
-        />
-      </Marquee>
+      <div className="w-3/4 mx-auto">
+        <Marquee gradient gradientColor="#121212">
+          <div className="flex space-x-15 ps-15">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+              height="60"
+              width="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+              height="60"
+              width="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              className="bg-stone-50"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+              width="60"
+              height="60"
+            />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+              width="60"
+              height="60"
+            />
+          </div>
+        </Marquee>
+      </div>
     </div>
   );
 };
