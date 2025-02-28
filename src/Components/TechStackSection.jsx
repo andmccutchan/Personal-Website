c00937feba8bed2e96e7c41b5850ce3fc52cbdc4
andmccutchan <andmccutchan2004@gmail.com>
2025-02-27 19:43:52 -0500

    Contact form

--- src/Components/TechStackSection.jsx
+++ src/Components/TechStackSection.jsx
@@ -44,81 +44,83 @@ const TechStackSection = () => {
   ];
 
   return (
-    <div className="w-full">
-      <div className="flex">
-        <motion.h2
-          className="secondary-font text-5xl text-stone-50 my-2 hover:cursor-pointer"
-          whileHover={{ scale: 1.1 }}
-        >
-          Technologies
-        </motion.h2>
+    <section>
+      <div className="w-full">
+        <div className="flex">
+          <motion.h2
+            className="secondary-font text-5xl text-stone-50 my-2 hover:cursor-pointer"
+            whileHover={{ scale: 1.1 }}
+          >
+            Technologies
+          </motion.h2>
+        </div>
+        <div className="grid grid-cols-3 gap-4 w-3/4 mx-auto">
+          {/* {technologies.map((tech, element) => (
+            <TechCard
+              key={element}
+              name={tech.name}
+              imgUrl={tech.imgUrl}
+              bio={tech.bio}
+            />
+          ))} */}
+          {/* <Marquee gradient gradientColor="#121212">
+            <div className="flex space-x-15 ps-15">
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+                height="60"
+                width="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+                height="60"
+                width="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                className="bg-stone-50 border rounded p-1"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+                width="60"
+                height="60"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+                width="60"
+                height="60"
+              />
+            </div>
+          </Marquee> */}
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-4 w-3/4 mx-auto">
-        {/* {technologies.map((tech, element) => (
-          <TechCard
-            key={element}
-            name={tech.name}
-            imgUrl={tech.imgUrl}
-            bio={tech.bio}
-          />
-        ))} */}
-        {/* <Marquee gradient gradientColor="#121212">
-          <div className="flex space-x-15 ps-15">
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-              height="60"
-              width="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
-              height="60"
-              width="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              className="bg-stone-50 border rounded p-1"
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
-              width="60"
-              height="60"
-            />
-            <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
-              width="60"
-              height="60"
-            />
-          </div>
-        </Marquee> */}
-      </div>
-    </div>
+    </section>
   );
 };
 
