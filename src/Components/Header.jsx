ca55440c25ca64a8c314f82b5745ffbbced27d7f
andmccutchan <andmccutchan2004@gmail.com>
2025-03-04 22:45:00 -0500

    Header fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "motion/react";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    console.log(showMenu);
+  };
+
   return (
     <nav className="flex w-full p-4 justify-center items-center text-gray-50 z-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between w-full mx-auto max-md:justify-end">
@@ -10,6 +16,7 @@ const Header = () => {
           <motion.button
             data-collapse-toggle="navbar-default"
             type="button"
+            onClick={toggleMenu}
             className="p-2 mx-2 w-10 h-10 flex items-center justify-center rounded-lg text-stone-50 hover:border-stone-50"
             aria-controls="navbar-default"
             aria-expanded="false"
@@ -35,8 +42,16 @@ const Header = () => {
         </div>
 
         {/* Desktop Navigation */}
-        <div className="hidden md:flex w-full justify-center">
-          <ul className="flex text-2xl gap-x-24 max-lg:gap-x-10">
+        <div
+          className={`flex w-full justify-center max-md:${
+            showMenu && "hidden"
+          } max-md:flex-col`}
+        >
+          <ul
+            className={`flex text-2xl gap-x-24 max-lg:gap-x-10 max-md:${
+              showMenu ? "flex-col" : "hidden"
+            }`}
+          >
             {["About Me", "Projects", "Tech Stack", "Contact Me"].map(
               (item, index) => (
                 <li key={index}>
